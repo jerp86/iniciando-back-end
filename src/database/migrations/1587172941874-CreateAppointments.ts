a9142aa4a2f08c4fec042835253c39c6b0a03529
jerp86 <jerp4@hotmail.com>
2020-04-18 19:41:49 -0300

    Relacionamento nos models

--- src/database/migrations/1587172941874-CreateAppointments.ts
+++ src/database/migrations/1587172941874-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587172941874
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
