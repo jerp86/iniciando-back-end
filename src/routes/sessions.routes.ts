841a610147ea39344094bb8ce9cc081e259d5702
jerp86 <jerp4@hotmail.com>
2020-04-18 21:50:28 -0300

    Validando credenciais

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthencicateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthencicateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
