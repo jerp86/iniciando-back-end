8e3409120906f89550d6887da96286f87a0da08a
jerp86 <jerp4@hotmail.com>
2020-04-18 23:25:11 -0300

    Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'reflect-metadata';
 import express from 'express';
 
 import routes from './routes';
+import uploadConfig from './config/uploads';
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.diretory));
 app.use(routes);
 
 app.listen(3333, () => {
