0186362dfb6fad7d4541180df78458ff43d70798
jerp86 <jerp4@hotmail.com>
2020-04-21 22:16:27 -0300

    Habilitando CORS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from './errors/AppError';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.diretory));
 app.use(routes);
