dcd78361ab90158670749b731490ceb1c5e74517
jerp86 <jerp4@hotmail.com>
2020-05-14 15:59:35 -0300

    Criação do agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,11 +17,12 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '123123',
+      provider_id: 'provider',
+      user_id: 'user',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123');
+    expect(appointment.provider_id).toBe('provider');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
@@ -29,13 +30,15 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123',
+      provider_id: 'provider',
+      user_id: 'user',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '123123',
+        provider_id: 'provider',
+        user_id: 'user',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,11 +16,13 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list the Day availability from provider', async () => {
     await fakeAppointmentRepository.create({
       provider_id: 'provider',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentRepository.create({
       provider_id: 'provider',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
